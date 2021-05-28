945190a221215763338732c4970133cc9118dd1b
ElektroCloud <ni.berger2004@gmail.com>
2021-05-28 23:53:11 +0200

    First Class

---
+++ src/main/java/net/theperfectgamers/elektrocloud/essentials/Essentials.java
@@ -0,0 +1,16 @@
+package net.theperfectgamers.elektrocloud.essentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Essentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
